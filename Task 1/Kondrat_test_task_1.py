dd8a4d3f1c81417564af57d395d22409721c15a8
archgear <mihajlokondrat@MacBook-Pro-Mihajlo.local>
2019-05-01 22:45:35 +0300

    Task 1

---
+++ Task 1/Kondrat_test_task_1.py
@@ -0,0 +1,14 @@
+#check on emptiness, is palindrome and revert
+def revert_string(txt):
+    # emptiness check
+    while not txt:
+        print('String is empty')
+        txt = input("Please enter a string to revert, not empty this time: ")
+    # palindrome check
+    return print('That\'s a palindrome, what did you expected?!')  if txt == txt[::-1] else print(txt[::-1])
+###############################
+# Asking user to type in string
+string = input("Please enter a string to revert: ")
+
+revert_string(string)
+
